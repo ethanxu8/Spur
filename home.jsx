2ee05c9856f7b58bf2ece2160b92c9774ffe298d
ethanxu8 <xuethan2004@gmail.com>
2024-07-08 00:11:39 -0600

    to-do list for Spur added

--- home.jsx
+++ home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { ReactDOM } from 'react'
 import './style.css';
 
-<<<<<<< HEAD
 export default function home() {
   return (
     <div>
@@ -10,8 +9,3 @@ export default function home() {
     </div>
   )
 }
-=======
-// Spur begins here!
-// Limit tweaking to once a day
-// Hooray
->>>>>>> 9aea8a0d6f90d29e46db042223bcb25a43fbc33f
